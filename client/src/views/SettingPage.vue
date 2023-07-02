d892d32281f91ef8762ae175439afe5bbdba3831
hoonloper <yonghoonj98@gmail.com>
2023-07-02 21:05:00 +0900

    Feat: 소켓 연결 기능 이동

--- client/src/views/SettingPage.vue
+++ client/src/views/SettingPage.vue
@@ -1,3 +1,17 @@
-<template><div>Setting</div></template>
+<template>
+  <div>
+    <h1>Setting</h1>
+    <button @click="logout">로그아웃</button>
+  </div>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import router from '@/router';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const logout = () => {
+  store.commit('clear');
+  router.push('sign-in');
+};
+</script>
