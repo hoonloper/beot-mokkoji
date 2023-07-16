61c929c8d16bc5a934cec440816e91cd8f183745
hoonloper <yonghoonj98@gmail.com>
2023-07-16 13:41:39 +0900

    Refactor: ExceptionController를 ExceptionHandler로 이름 변경

--- server/src/main/java/com/example/server/application/handler/ExceptionHandler.java
+++ server/src/main/java/com/example/server/application/handler/ExceptionHandler.java
@@ -1,19 +1,18 @@
-package com.example.server.application.controllers;
+package com.example.server.application.handler;
 
 import com.example.server.application.exceptions.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 @Slf4j
-public class ExceptionController {
+public class ExceptionHandler {
     /* Validator Exception */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ExceptionError handleValidException(final MethodArgumentNotValidException ex) {
@@ -21,7 +20,7 @@ public class ExceptionController {
         return buildExceptionError(ex, HttpStatus.BAD_REQUEST);
     }
     /* 400 - Bad Request */
-    @ExceptionHandler(BadRequestException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ExceptionError handleBadRequest(final RuntimeException ex) {
@@ -30,7 +29,7 @@ public class ExceptionController {
     }
 
     /* 401 - Unauthorized */
-    @ExceptionHandler(UnauthorizedException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(UnauthorizedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ResponseBody
     public ExceptionError handleUnauthorized(final RuntimeException ex) {
@@ -39,7 +38,7 @@ public class ExceptionController {
     }
 
     /* 403 - Forbidden */
-    @ExceptionHandler(ForbiddenException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(ForbiddenException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ResponseBody
     public ExceptionError handleForbidden(final RuntimeException ex) {
@@ -48,7 +47,7 @@ public class ExceptionController {
     }
 
     /* 404 - Not Found */
-    @ExceptionHandler(NotFoundException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
     public ExceptionError handleNotFound(final RuntimeException ex) {
@@ -58,7 +57,7 @@ public class ExceptionController {
 
 
     /* 500 - Internal Server Error(and ALL) */
-    @ExceptionHandler({ Exception.class })
+    @org.springframework.web.bind.annotation.ExceptionHandler({ Exception.class })
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public ExceptionError handleServerError(final Exception ex) {
@@ -68,7 +67,7 @@ public class ExceptionController {
     }
 
     /* 알 수 없는 에러 */
-    @ExceptionHandler(RuntimeException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public ExceptionError handleUnknownException(final RuntimeException ex) {
