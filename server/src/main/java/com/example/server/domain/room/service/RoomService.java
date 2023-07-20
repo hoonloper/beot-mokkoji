7f667e9911ad796c608ed54343eb12aedef3426b
hoonloper <yonghoonj98@gmail.com>
2023-07-20 19:42:57 +0900

    Feat: RoomDto class에서 record 빌더 패턴으로 변경

--- server/src/main/java/com/example/server/domain/room/service/RoomService.java
+++ server/src/main/java/com/example/server/domain/room/service/RoomService.java
@@ -60,7 +60,7 @@ public class RoomService {
     }
     public RoomVo createRoom(RoomDto roomDto) {
         String roomId = UUID.randomUUID().toString(); // 랜덤한 방 아이디 생성
-        RoomVo room = RoomVo.toRoomVo(roomRepository.save(new Room(roomDto.getMemberId(), roomId, roomDto.getName())));
+        RoomVo room = RoomVo.toRoomVo(roomRepository.save(new Room(roomDto.memberId(), roomId, roomDto.name())));
         registerRoom(room); // 랜덤 아이디와 room 정보를 Map 에 저장
         return room;
     }
