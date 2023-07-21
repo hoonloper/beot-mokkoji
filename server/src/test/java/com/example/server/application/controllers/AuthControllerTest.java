bfa87ec459bbd495f61ae8265a83836a68cafb8d
hoonloper <yonghoonj98@gmail.com>
2023-07-21 20:06:27 +0900

    Test: AuthControllerTest 테스트 코드 개선

--- server/src/test/java/com/example/server/application/controllers/AuthControllerTest.java
+++ server/src/test/java/com/example/server/application/controllers/AuthControllerTest.java
@@ -2,9 +2,11 @@ package com.example.server.application.controllers;
 
 import com.example.server.domains.member.dto.MemberDto;
 import com.example.server.domains.member.service.MemberService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -40,23 +42,36 @@ public class AuthControllerTest {
     @Nested
     @DisplayName("성공 케이스")
     class Success {
+        String uuid;
+        ObjectMapper mapper;
+        MemberDto memberDto;
+        String json;
+        MemberDto returnValue;
+        @BeforeEach
+        void init() throws JsonProcessingException {
+            String EXPECTED_NAME = "이름";
+            String EXPECTED_NICKNAME = "닉네임";
+            LocalDate EXPECTED_BIRTHDAY = LocalDate.of(2022, Month.JUNE, 30);
+            memberDto = new MemberDto(null, EXPECTED_NAME, EXPECTED_NICKNAME, EXPECTED_BIRTHDAY);
+            uuid = UUID.randomUUID().toString();
+            mapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            json = mapper.writeValueAsString(memberDto);
+            returnValue = new MemberDto(uuid, EXPECTED_NAME, EXPECTED_NICKNAME, EXPECTED_BIRTHDAY);
+        }
+
         @Test
         @DisplayName("회원가입 API")
         void signUp() throws Exception {
-            String uuid = UUID.randomUUID().toString();
-            given(memberService.signUp(any())).willReturn(new MemberDto(uuid, "이름", "닉네임", LocalDate.of(2022, Month.JUNE, 30)));
-
-            MemberDto memberDto = new MemberDto(null, "이름", "닉네임", LocalDate.of(2022, Month.JUNE, 30));
-            ObjectMapper mapper = new ObjectMapper()
-                    .registerModule(new JavaTimeModule())
-                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            String json = mapper.writeValueAsString(memberDto);
+            given(memberService.signUp(any())).willReturn(returnValue);
 
             mvc.perform(MockMvcRequestBuilders.post(END_POINT + "/sign-up")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(json)
                     .accept(MediaType.APPLICATION_JSON)
-            ).andDo(print()).andExpect(MockMvcResultMatchers.status().isCreated())
+            ).andDo(print())
+                    .andExpect(MockMvcResultMatchers.status().isCreated())
                     .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(uuid))
                     .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("이름"))
                     .andExpect(MockMvcResultMatchers.jsonPath("$.nickname").value("닉네임"))
@@ -65,20 +80,14 @@ public class AuthControllerTest {
         @Test
         @DisplayName("로그인 API")
         void signIn() throws Exception {
-            String uuid = UUID.randomUUID().toString();
-            given(memberService.signIn(any())).willReturn(new MemberDto(uuid, "이름", "닉네임", LocalDate.of(2022, Month.JUNE, 30)));
+            given(memberService.signIn(any())).willReturn(returnValue);
 
-            MemberDto memberDto = new MemberDto(null, "이름", "닉네임", LocalDate.of(2022, Month.JUNE, 30));
-            ObjectMapper mapper = new ObjectMapper()
-                    .registerModule(new JavaTimeModule())
-                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            String json = mapper.writeValueAsString(memberDto);
-
-            ResultActions resultActions = mvc.perform(MockMvcRequestBuilders.post(END_POINT + "/sign-in")
+            mvc.perform(MockMvcRequestBuilders.post(END_POINT + "/sign-in")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(json)
                     .accept(MediaType.APPLICATION_JSON)
-            ).andDo(print()).andExpect(MockMvcResultMatchers.status().isOk())
+            ).andDo(print())
+                    .andExpect(MockMvcResultMatchers.status().isOk())
                     .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(uuid))
                     .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("이름"))
                     .andExpect(MockMvcResultMatchers.jsonPath("$.nickname").value("닉네임"))
