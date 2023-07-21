b7562d276459a630542e4590938c372f9378d287
hoonloper <yonghoonj98@gmail.com>
2023-07-21 19:58:08 +0900

    Test: 회원가입 테스트 코드 작성 및 코드 개선

--- server/src/test/java/com/example/server/application/controllers/AuthControllerTest.java
+++ server/src/test/java/com/example/server/application/controllers/AuthControllerTest.java
@@ -16,10 +16,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 public class AuthControllerTest {
     @MockBean
     private MemberService memberService;
+
     @Autowired
     MockMvc mvc;
 
@@ -36,24 +41,48 @@ public class AuthControllerTest {
     @DisplayName("성공 케이스")
     class Success {
         @Test
+        @DisplayName("회원가입 API")
+        void signUp() throws Exception {
+            String uuid = UUID.randomUUID().toString();
+            given(memberService.signUp(any())).willReturn(new MemberDto(uuid, "이름", "닉네임", LocalDate.of(2022, Month.JUNE, 30)));
+
+            MemberDto memberDto = new MemberDto(null, "이름", "닉네임", LocalDate.of(2022, Month.JUNE, 30));
+            ObjectMapper mapper = new ObjectMapper()
+                    .registerModule(new JavaTimeModule())
+                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            String json = mapper.writeValueAsString(memberDto);
+
+            mvc.perform(MockMvcRequestBuilders.post(END_POINT + "/sign-up")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json)
+                    .accept(MediaType.APPLICATION_JSON)
+            ).andDo(print()).andExpect(MockMvcResultMatchers.status().isCreated())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(uuid))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("이름"))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.nickname").value("닉네임"))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.birthday").value("2022-06-30"));
+        }
+        @Test
         @DisplayName("로그인 API")
         void signIn() throws Exception {
-            // given
+            String uuid = UUID.randomUUID().toString();
+            given(memberService.signIn(any())).willReturn(new MemberDto(uuid, "이름", "닉네임", LocalDate.of(2022, Month.JUNE, 30)));
+
             MemberDto memberDto = new MemberDto(null, "이름", "닉네임", LocalDate.of(2022, Month.JUNE, 30));
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.registerModule(new JavaTimeModule());
-            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            ObjectMapper mapper = new ObjectMapper()
+                    .registerModule(new JavaTimeModule())
+                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             String json = mapper.writeValueAsString(memberDto);
-            System.out.println(json);
-            // when
+
             ResultActions resultActions = mvc.perform(MockMvcRequestBuilders.post(END_POINT + "/sign-in")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(json)
                     .accept(MediaType.APPLICATION_JSON)
-            );
-
-            // then
-            resultActions.andDo(print());
+            ).andDo(print()).andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(uuid))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("이름"))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.nickname").value("닉네임"))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.birthday").value("2022-06-30"));;
         }
     }
 }
