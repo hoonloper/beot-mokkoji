c6f35aad62b80b476b499df9fa81fe9edf6ba4de
hoonloper <yonghoonj98@gmail.com>
2023-07-24 18:15:19 +0900

    Refactor: H2DatabaseConfig를 DatabaseConfig로 수정

--- server/src/main/java/com/example/server/application/configs/DatabaseConfig.java
+++ server/src/main/java/com/example/server/application/configs/DatabaseConfig.java
@@ -10,7 +10,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @Component
-public class H2DatabaseConfig implements ApplicationRunner {
+public class DatabaseConfig implements ApplicationRunner {
     @Autowired
     DataSource dataSource;
 
